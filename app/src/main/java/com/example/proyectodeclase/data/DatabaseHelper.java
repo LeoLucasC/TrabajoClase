7ac2f9e29dd4c14ecac80636dbef1f3386060ad4
LeoLucasC <gloriaxd77@hotmail.com>
2025-02-28 12:19:38 -0500

    mensaje avance

--- app/src/main/java/com/example/proyectodeclase/data/DatabaseHelper.java
+++ app/src/main/java/com/example/proyectodeclase/data/DatabaseHelper.java
@@ -1,8 +1,15 @@
 package com.example.proyectodeclase.data;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import com.example.proyectodeclase.Contacto;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
@@ -48,4 +55,53 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         SQLiteDatabase db = this.getReadableDatabase();
         return db != null && db.isOpen();
     }
+
+
+
+    public long insertContacto(String nombre, String numero) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_NOMBRE, nombre);
+        values.put(COLUMN_NUMERO, numero);
+        long id = db.insert(TABLE_CONTACTOS, null, values);
+        db.close();
+        return id;
+    }
+
+    // Método para obtener todos los contactos como un Cursor
+    public Cursor getAllContactos() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        return db.query(TABLE_CONTACTOS, null, null, null, null, null, null);
+    }
+
+    // Método para obtener todos los contactos como una lista de objetos Contacto
+    public List<Contacto> obtenerContactos() {
+        List<Contacto> listaContactos = new ArrayList<>();
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.query(TABLE_CONTACTOS, null, null, null, null, null, null);
+
+        if (cursor != null && cursor.moveToFirst()) {
+            do {
+                int id = cursor.getInt(cursor.getColumnIndexOrThrow(COLUMN_ID));
+                String nombre = cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_NOMBRE));
+                String numero = cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_NUMERO));
+                listaContactos.add(new Contacto(id, nombre, numero));
+            } while (cursor.moveToNext());
+            cursor.close();
+        }
+
+        db.close();
+        return listaContactos;
+    }
+
+
+    // Método para eliminar un contacto por ID
+    public int eliminarContacto(long id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete(TABLE_CONTACTOS, COLUMN_ID + " = ?", new String[]{String.valueOf(id)});
+    }
+
+
+
+
 }
